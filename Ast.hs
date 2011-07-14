c69a124f5f2e8be5f6eb7fba165f7756f54b365c
Zachary Palmer <zep_git@bahj.com>
2011-07-14 18:19:17 -0400

    Expanded and refactored AST type definition.

--- Ast.hs
+++ Ast.hs
@@ -17,12 +17,19 @@ data Expr =
     | PrimInt Integer
     | PrimString String
     | PrimUnit
-    | Case Expr [(Chi,Expr)]
+    | Case Expr Branches
+    -- Below are the AST forms which cannot be represented as text directly
+    | Plus Expr Expr
+    | Minus Expr Expr
+    | Equal Expr Expr
 
 -- |Data type describing type patterns for case expressions.
 data Chi =
       ChiPrim (T.PrimitiveType)
-    | ChiLabel Ident
+    | ChiLabel LabelName Ident
     | ChiOnion Ident Ident
     | ChiFun
 
+-- |Alias for case branches
+type Branches = [Branch]
+type Branch = (Chi,Expr)
