7e156a40ae8a0a7c332ae67cb229f00c0fdff0ec
Zachary Palmer <zep_git@bahj.com>
2011-07-14 18:20:53 -0400

    Added some Show derivations to utility types.

--- UtilTypes.hs
+++ UtilTypes.hs
@@ -12,11 +12,12 @@ module UtilTypes
 
 -- |A distinguished type for labels.
 newtype LabelName = LabelName { unLabelName :: String }
-    deriving (Eq, Ord)
+    deriving (Eq, Ord, Show)
 {- TODO: smarter constructor -}
 labelName s = LabelName s
 
 -- |A distinguished type for identifiers.
 newtype Ident = Ident { unIdent :: String }
+    deriving (Eq, Ord, Show)
 {- TODO: smarter constructor -}
 ident s = Ident s
