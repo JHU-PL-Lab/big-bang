ea12835515317707ea632f1348bc8e21e12f4122
Samuel Griff <slgriff@smcm.edu>
2011-07-25 12:01:56 -0400

    Removed outdated Lexer_Test file.

--- Language/BigBang/Syntax/Lexer_Test.hs
+++
@@ -1,8 +0,0 @@
-module LexerTest where
-
-import Language.BigBang.Syntax.Lexer( Token(..), lexBigBang )
-import Test.HUnit
-
-testEmpty = TestCase $ assertEqual Nothing ( alexGetChar ('', [])  )
-
-main = runTestTT testEmpty
