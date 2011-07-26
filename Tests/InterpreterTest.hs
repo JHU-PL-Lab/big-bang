a1cb6c74d1e770e56bb7ed15bd56d42b74e3effd
Samuel Griff <slgriff@smcm.edu>
2011-07-26 13:22:20 -0400

    added tests for case...of blocks

--- Tests/InterpreterTest.hs
+++ Tests/InterpreterTest.hs
@@ -2,12 +2,13 @@ module InterpreterTest where
 
 import Test.HUnit hiding (Label)
 import Language.BigBang.Ast
+import qualified Language.BigBang.Types.Types as T
 import Language.BigBang.Types.UtilTypes
 import Language.BigBang.Interpreter.Interpreter
 import Language.BigBang.Syntax.Parser
 import Language.BigBang.Syntax.Lexer
 
-edgeCases = TestList [testUnbound, testTypeMismatch, testInvalidApplication]
+edgeCases = TestList [testNonexhaustiveCases, testUnbound, testTypeMismatch, testInvalidApplication]
 
 testUnbound = TestCase $ assertEqual
   "Test if evaluation of free variable causes error"
@@ -24,7 +25,12 @@ testInvalidApplication = TestCase $ assertEqual
   (Left (ApplNotFunction (PrimInt 1) (PrimChar 'x')))
   (evalTop $ parseBigBang $ lexBigBang "1 \'x\'")
 
-simpleCases = TestList [testInterpretInt, testInterpretChar, testPlusInt, testLambdaAppl, testFuncAppl]
+testNonexhaustiveCases = TestCase $ assertEqual
+  "Test if nonexhaustive cases throws an error"
+  (Left (UnmatchedCase (PrimInt 1) [(ChiPrim T.PrimChar,PrimInt 0)])) 
+  (evalTop $ parseBigBang $ lexBigBang "case 1 of {\nchar -> 0}")
+
+simpleCases = TestList [testCaseChar, testCaseInt, testCaseFun, testCaseLambda, testCaseUnit, testInterpretInt, testInterpretChar, testPlusInt, testLambdaAppl, testFuncAppl]
 
 testInterpretInt = TestCase $ assertEqual
   "Test if input 1234567890 is interpreted correctly"
@@ -50,6 +56,31 @@ testFuncAppl = TestCase $ assertEqual
   "Test if function definitions are applied correctly"
   (Right (PrimInt 4))
   (evalTop $ parseBigBang $ lexBigBang "(fun x -> plus x x) 2")
+
+testCaseChar = TestCase $ assertEqual
+  "Test if case matches characters correctly"
+  (Right (PrimInt 0))
+  (evalTop $ parseBigBang $ lexBigBang "case 'a' of {\nchar -> 0}")
+
+testCaseInt = TestCase $ assertEqual
+  "Test if case matches integers correctly"
+  (Right (PrimInt 0))
+  (evalTop $ parseBigBang $ lexBigBang "case 1234567890 of {\nint -> 0}")
+
+testCaseFun = TestCase $ assertEqual
+  "Test if case matches functions correctly"
+  (Right (PrimInt 0))
+  (evalTop $ parseBigBang $ lexBigBang "case (fun x -> x) of {\nfun -> 0}")
+
+testCaseLambda = TestCase $ assertEqual
+  "Test if case matches lambdas correctly"
+  (Right (PrimInt 0))
+  (evalTop $ parseBigBang $ lexBigBang "case (\\x -> x) of {\nfun -> 0}")
+
+testCaseUnit = TestCase $ assertEqual
+  "Test if case matches units correctly"
+  (Right (PrimInt 0))
+  (evalTop $ parseBigBang $ lexBigBang "case () of {\nunit -> 0}")
 
 tests = TestList [edgeCases, simpleCases]
 main = runTestTT tests
