7f80cc865b19c76b55ddbdf789ec6f7a54576929
Samuel Griff <slgriff@smcm.edu>
2011-07-28 11:29:14 -0400

    added test for function nesting and equality

--- Tests/Language/BigBang/Interpreter/Interpreter.hs
+++ Tests/Language/BigBang/Interpreter/Interpreter.hs
@@ -10,7 +10,7 @@ import Language.BigBang.Interpreter.Interpreter
 import Language.BigBang.Syntax.Parser
 import Language.BigBang.Syntax.Lexer
 
-edgeCases = TestList [testEqualTypeMismatch, testNonexhaustiveCases, testUnbound, testTypeMismatch, testInvalidApplication]
+edgeCases = TestList [testEqualUnit, testEqualTypeMismatch, testNonexhaustiveCases, testUnbound, testTypeMismatch, testInvalidApplication]
 
 testUnbound = TestCase $ assertEqual
   "Test if evaluation of free variable causes error"
@@ -37,7 +37,17 @@ testNonexhaustiveCases = TestCase $ assertEqual
   (Left (UnmatchedCase (PrimInt 1) [(ChiPrim T.PrimChar,PrimInt 0)])) 
   (evalTop $ parseBigBang $ lexBigBang "case 1 of {\nchar -> 0}")
 
-simpleCases = TestList [testEqualLabel, testEqualChar, testEqualInt, testMinusInt, testCaseLabel, testCaseChar, testCaseInt, testCaseFun, testCaseLambda, testCaseUnit, testInterpretInt, testInterpretChar, testPlusInt, testLambdaAppl, testFuncAppl]
+testEqualUnit = TestCase $ assertEqual
+  "equal () () should throw a type error"
+  (Left (DynamicTypeError "incorrect type in expression"))
+  (evalTop $ parseBigBang $ lexBigBang "equal () ()")
+
+simpleCases = TestList [testOnionIsomorphism, testOnionNotEqual, testOnionNotEqual2, testOnionNotEqual3, testEqualBoolean, testEqualChar, testEqualInt, testMinusInt, testCaseLabel, testCaseChar, testCaseInt, testCaseFun, testCaseLambda, testCaseUnit, testInterpretInt, testInterpretChar, testPlusInt, testLambdaAppl, testFuncAppl]
+
+testFunctionNesting = TestCase $ assertEqual
+  "Test if function nesting works correctly"
+  (Right (PrimInt 4))
+  (evalTop $ parseBigBang $ lexBigBang "plus (minus 1 -1) (minus 1 -1)")
 
 testInterpretInt = TestCase $ assertEqual
   "Test if input 1234567890 is interpreted correctly"
@@ -71,8 +81,12 @@ testEqualInt = TestCase $ do
     (evalTop $ parseBigBang $ lexBigBang "equal 1 1")
   assertEqual
     "equal 0 1 should return false"
-    (Right (Label (labelName "False")PrimUnit))
+    (Right (Label (labelName "False") PrimUnit))
     (evalTop $ parseBigBang $ lexBigBang "equal 0 1")
+  assertEqual
+    "equal 0 (minus (minus (plus 1 1) 1) 1) should return true"
+    (Right (Label (labelName "True") PrimUnit))
+    (evalTop $ parseBigBang $ lexBigBang "equal 0 (minus (minus (plus 1 1) 1) 1)")
 
 testEqualChar = TestCase $ do
   assertEqual
@@ -84,7 +98,7 @@ testEqualChar = TestCase $ do
     (Right (Label (labelName "False") PrimUnit))
     (evalTop $ parseBigBang $ lexBigBang "equal \'a\' \'A\'")
 
-testEqualLabel = TestCase $ do
+testEqualBoolean = TestCase $ do
   assertEqual
     "equal `True () `True () should return true"
     (Right (Label (labelName "True") PrimUnit))
@@ -93,6 +107,41 @@ testEqualLabel = TestCase $ do
     "equal `True () `False () should return false"
     (Right (Label (labelName "True") PrimUnit))
     (evalTop $ parseBigBang $ lexBigBang "equal `True () `False ()")
+
+testEqualLabel = TestCase $ assertEqual
+  "equal `A 1 `A 1 should return true"
+  (Right (Label (labelName "True") PrimUnit))
+  (evalTop $ parseBigBang $ lexBigBang "equal `A 1 `A 1")
+
+testEqualLabel2 = TestCase $ assertEqual
+  "equal `A 1 `B 1 should throw a type error"
+  (Left (DynamicTypeError "incorrect type in expression"))
+  (evalTop $ parseBigBang $ lexBigBang "equal `A 1 `B 1")
+
+testOnionIsomorphism = TestCase $ do
+  assertEqual
+    "equal (1 & \'a\') (\'a\' & 1) should return true"
+    (Right (Label (labelName "True") PrimUnit))
+    (evalTop $ parseBigBang $ lexBigBang "equal (1 & \'a\') (\'a\' & 1)")
+  assertEqual
+    "equal (\'a\' & 1) (1 & \'a\') should return true"
+    (Right (Label (labelName "True") PrimUnit))
+    (evalTop $ parseBigBang $ lexBigBang "equal (\'a\' & 1) (1 & \'a\')")
+
+testOnionNotEqual = TestCase $ assertEqual
+  "equal (1 & \'a\') (1 & \'z\') should return false"
+  (Right (Label (labelName "False") PrimUnit))
+  (evalTop $ parseBigBang $ lexBigBang "equal (1 & \'a\') (1 & \'z\')")
+
+testOnionNotEqual2 = TestCase $ assertEqual
+  "equal (1 & \'a\') (0 & \'a\') should return false"
+  (Right (Label (labelName "False") PrimUnit))
+  (evalTop $ parseBigBang $ lexBigBang "equal (1 & \'a\') (0 & \'a\')")
+
+testOnionNotEqual3 = TestCase $ assertEqual
+  "equal (1 & 2) (2 & 1) should return false"
+  (Right (Label (labelName "False") PrimUnit))
+  (evalTop $ parseBigBang $ lexBigBang "equal (1 & 2) (2 & 1)")
 
 testLambdaAppl = TestCase $ assertEqual
   "Test if lambda functions are applied correctly"
