afe54a9b53072e301109a65b33931db10a647a80
Alex Rozenshteyn <rpglover64@gmail.com>
2011-08-01 16:23:11 -0400

    Initial commit of Closure rules. Transitivity appears to work.

---
+++ src/Language/BigBang/Types/Closure.hs
@@ -0,0 +1,64 @@
+module Language.BigBang.Types.Closure
+(
+)
+where
+
+import qualified Language.BigBang.Types.Types as T
+import Language.BigBang.Types.Types ((<:))
+import Language.BigBang.Types.Types (Constraints)
+import Language.BigBang.Types.UtilTypes (LabelName)
+
+import Data.Set (Set)
+import qualified Data.Set as Set
+import Data.Maybe (catMaybes)
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Data.Monoid (mappend)
+
+topLevelAssignable :: T.TauDownClosed -> T.TauChi -> Bool
+topLevelAssignable tau chi = True
+
+findTauDownOpen :: Constraints -> Constraints
+findTauDownOpen = Set.fromAscList . catMaybes . map fn . Set.toAscList
+  where fn c =
+          case c of
+            T.Subtype (T.TdcAlpha _) _ -> Nothing
+            _                          -> Just c
+
+findAlphaOnRight :: Constraints -> Map T.Alpha (Set T.TauDownClosed)
+findAlphaOnRight = Map.unionsWith mappend . map fn . Set.toList
+  where fn c =
+          case c of
+            T.Subtype a (T.TucAlpha b) -> Map.singleton b $ Set.singleton a
+            _                          -> Map.empty
+
+findAlphaOnLeft :: Constraints -> Map T.Alpha (Set T.TauUpClosed)
+findAlphaOnLeft = Map.unionsWith mappend . map fn . Set.toAscList
+  where fn c = 
+          case c of
+            T.Subtype (T.TdcAlpha a) b -> Map.singleton a $ Set.singleton b
+            _                          -> Map.empty
+
+findLblAlphaOnLeft :: Constraints -> Map T.Alpha (Set (LabelName, T.TauUpClosed))
+findLblAlphaOnLeft = Map.unionsWith mappend . map fn . Set.toAscList
+  where fn c = 
+          case c of
+            T.Subtype (T.TdcLabel lbl (T.TdcAlpha a)) b ->
+              Map.singleton a $ Set.singleton (lbl, b)
+            _ -> Map.empty
+
+transitivity :: Constraints -> Constraints
+transitivity cs = Set.fromList $
+                  concat $
+                  Map.elems $
+                  Map.intersectionWith subtypeCrossProduct lefts rights
+  where relevantCs = findTauDownOpen cs
+        lefts      = findAlphaOnRight relevantCs
+        rights     = findAlphaOnLeft  cs
+        subtypeCrossProduct xs ys =
+          [ x <: y | x <- Set.toList xs, y <- Set.toList ys ]
+
+--labels :: Constraints -> Constraints
+--labels cs = 
+--  where relevantCs = findTauDownOpen cs
+--        lefts      = findAlphaOnRight relevantCs
