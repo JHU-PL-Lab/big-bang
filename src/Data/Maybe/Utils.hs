29bdd558971eb2392b6cf9bf8ba3a1a6785dad8d
Zachary Palmer <zep_git@bahj.com>
2011-08-03 16:29:07 -0400

    Added maybe utilities.

---
+++ src/Data/Maybe/Utils.hs
@@ -0,0 +1,8 @@
+module Data.Maybe.Utils
+( justIf
+) where
+
+-- |A utility function for producing a Maybe value based on a boolean.
+justIf :: a -> Bool -> Maybe a
+justIf v b = if b then Just v else Nothing
+
