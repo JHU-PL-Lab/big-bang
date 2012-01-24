f417ed19e4ec5f642447c441491593540d518a23
Alex Rozenshteyn <rpglover64@gmail.com>
2012-01-24 10:39:58 -0500

    Add handling of empty onions and subtraction to interpreter.
    Remove deprecated onion equality code.
    Adjust indentation.

--- src/Language/TinyBang/Interpreter/Interpreter.hs
+++ src/Language/TinyBang/Interpreter/Interpreter.hs
@@ -24,6 +24,7 @@ import Language.TinyBang.Ast
   , Value(..)
   , LazyOperator(..)
   , EagerOperator(..)
+  , Sigma(..)
   , exprFromValue
   )
 import Language.TinyBang.Render.Display
@@ -36,6 +37,7 @@ import Language.TinyBang.Types.UtilTypes
     , labelName
 --    , unLabelName
     )
+import Control.Applicative ((<$>))
 
 -- TODO: remove
 -- import Debug.Trace
@@ -46,23 +48,28 @@ data EvalError =
     | DynamicTypeError String -- TODO: figure out what goes here
     | NotClosed Ident
     | UnmatchedCase Value Branches
+    | IllegalComparison EagerOperator Value Value
     deriving (Eq, Show)
 instance Error EvalError where
-    strMsg = error
+  strMsg = error
 instance Display EvalError where
-    makeDoc ee = case ee of
-        ApplNotFunction e e' ->
-            text "Attempted to apply" <+> makeDoc e <+> text "to" <+>
-            makeDoc e' <+> text "but the prior is not a function"
-        DynamicTypeError str ->
-            -- TODO: not a string!
-            text "Dynamic type error:" <+> text str
-        NotClosed i ->
-            text "Expression not closed for variable" <+> (text $ unIdent i)
-        UnmatchedCase e brs ->
-            text "Case of" <+> makeDoc e <+>
-            text "cannot be matched by branches" $$
-            (nest 4 $ makeDoc brs)
+  makeDoc ee =
+    case ee of
+      ApplNotFunction e e' ->
+        text "Attempted to apply" <+> makeDoc e <+> text "to" <+>
+        makeDoc e' <+> text "but the prior is not a function"
+      DynamicTypeError str ->
+        -- TODO: not a string!
+        text "Dynamic type error:" <+> text str
+      NotClosed i ->
+        text "Expression not closed for variable" <+> (text $ unIdent i)
+      UnmatchedCase e brs ->
+        text "Case of" <+> makeDoc e <+>
+        text "cannot be matched by branches" $$
+        (nest 4 $ makeDoc brs)
+      IllegalComparison op v1 v2 ->
+        text "The comparison" <+> makeDoc op <+> makeDoc v1 <+> makeDoc v2 <+>
+        text "cannot be is not well typed."
 
 type EvalM = Either EvalError Value
 type CoerceTo a = Value -> Maybe a
@@ -133,40 +140,60 @@ eval (PrimChar c) = return $ VPrimChar c
 eval PrimUnit = return $ VPrimUnit
 
 eval (Case e branches) = do
-    e' <- eval e
-    let answers = catMaybes $ map (evalBranch e') branches
-    case answers of
-        [] -> throwError $ UnmatchedCase e' branches
-        answer:_ -> eval answer
-    where evalBranch e' (Branch mbinder chi branchExpr) =
-             let mval = coerce chi e'
-                 boundExpr = do
-                   val <- mval
-                   return $ maybe id (\binder -> subst (exprFromValue val) binder) mbinder $
+  e' <- eval e
+  let answers = catMaybes $ map (evalBranch e') branches
+  case answers of
+    [] -> throwError $ UnmatchedCase e' branches
+    answer:_ -> eval answer
+  where evalBranch e' (Branch mbinder chi branchExpr) =
+          let mval = coerce chi e'
+              boundExpr = do
+                val <- mval
+                return $ maybe id (\b -> subst (exprFromValue val) b) mbinder $
                          branchExpr
-             in
-             case (chi, mval) of
-                 -- We don't care about the matching the names because it was
-                 -- ensured to be correct earlier.
-                 (ChiLabel _ i, Just (VLabel _ lblVal)) ->
-                   fmap (subst (exprFromValue lblVal) i) boundExpr
-                 _ -> boundExpr
-          coerce chi =
-             case chi of
-                 ChiPrim T.PrimInt -> coerceToInteger
-                 ChiPrim T.PrimChar -> coerceToCharacter
-                 ChiPrim T.PrimUnit -> coerceToUnit
-                 ChiLabel name _ -> coerceToLabel name
-                 ChiFun -> coerceToFunction
-                 ChiAny -> Just
+          in
+          case (chi, mval) of
+            -- We don't care about the matching the names because it was
+            -- ensured to be correct earlier.
+            (ChiLabel _ i, Just (VLabel _ lblVal)) ->
+              fmap (subst (exprFromValue lblVal) i) boundExpr
+            _ -> boundExpr
+        coerce chi =
+          case chi of
+            ChiPrim T.PrimInt -> coerceToInteger
+            ChiPrim T.PrimChar -> coerceToCharacter
+            ChiPrim T.PrimUnit -> coerceToUnit
+            ChiLabel name _ -> coerceToLabel name
+            ChiFun -> coerceToFunction
+            ChiAny -> Just
+
+eval (OnionSub e s) = do
+  v <- eval e
+  return $ onionSub v
+  where onionSub v =
+          case (v, s) of
+            (VOnion v1 v2, _) -> onion (onionSub v1) (onionSub v2)
+            (VPrimInt _, SubPrim T.PrimInt) -> VEmptyOnion
+            (VPrimChar _, SubPrim T.PrimChar) -> VEmptyOnion
+            (VPrimUnit, SubPrim T.PrimUnit) -> VEmptyOnion
+            (VFunc _ _, SubFunc) -> VEmptyOnion
+            (VLabel n _, SubLabel n') | n == n' -> VEmptyOnion
+            _ -> v
+        onion VEmptyOnion v = v
+        onion v VEmptyOnion = v
+        onion v1 v2 = VOnion v1 v2
 
 eval (LazyOp e1 op e2) =
-    evalBinop e1 e2 tryExtractInteger $
-      case op of
-        Plus  -> \x y -> VPrimInt $ x + y
-        Minus -> \x y -> VPrimInt $ x - y
+  evalBinop e1 e2 tryExtractInteger $
+    case op of
+      Plus  -> \x y -> VPrimInt $ x + y
+      Minus -> \x y -> VPrimInt $ x - y
 
-eval (EagerOp e1 op e2) = error "Eager operations are not implemented yet"
+eval (EagerOp e1 op e2) = error "Eager operations are not implemented yet" $
+  case op of
+    Equal -> undefined
+    LessEqual -> undefined
+    GreaterEqual -> undefined
 
 -- eval (Equal e1 e2) = do
 --     e1' <- eval e1
@@ -201,6 +228,7 @@ eval (EagerOp e1 op e2) = error "Eager operations are not implemented yet"
 flattenOnion :: Value -> [Value]
 flattenOnion e =
   case e of
+    VEmptyOnion -> []
     VOnion e1 e2 -> flattenOnion e1 ++ flattenOnion e2
 -- Removed because I think that with it present, it invokes that
 -- annoying onion equality property that we can never remember is gone.
@@ -211,67 +239,42 @@ flattenOnion e =
 --  no type duplicates.
 canonicalizeList :: [Value] -> [Value]
 canonicalizeList xs = map last ys
-  where ys = groupBy ((==) `on` intFromType) $ sortBy (compare `on` intFromType) xs
+  where ys = groupBy eqValues $ sortBy compareValues xs
 
---TODO: come up with a more robust solution; perhaps a newtype?
--- |Convert type constructor to integer.
-intFromType :: Value -> Int
-intFromType v =
+onionListLessEq _ [] _  = Just True
+onionListLessEq _ _  [] = Nothing
+onionListLessEq cmp (x:xs) (y:ys) =
+  case compareValues x y of
+    LT -> onionListLessEq cmp xs (y:ys)
+    EQ -> (&& cmp x y) <$> onionListLessEq cmp xs ys
+    GT -> onionListLessEq cmp (x:xs) ys
+
+data ValueOrdinal
+  = OrdLabel LabelName
+  | OrdFunc
+  | OrdPrimInt
+  | OrdPrimChar
+  | OrdPrimUnit
+  deriving (Eq, Ord)
+
+leqValues = (<=) `on` valueToOrd
+compareValues = compare `on` valueToOrd
+eqValues = (==) `on` valueToOrd
+
+valueToOrd v =
   case v of
-    VLabel    _ _ -> 1
-    VOnion    _ _ -> 2
-    VFunc     _ _ -> 3
-    VPrimInt  _   -> 4
-    VPrimChar _   -> 5
-    VPrimUnit     -> 6
+    VLabel n _ -> OrdLabel n
+    VFunc _ _ -> OrdFunc
+    VPrimInt _ -> OrdPrimInt
+    VPrimChar _ -> OrdPrimChar
+    VPrimUnit -> OrdPrimUnit
+    _ -> error "This value should not be inside an onion"
 
 onionEq o1 o2 = c o1 == c o2
   where c = canonicalizeList . flattenOnion
 
 onionValueEq o v = c o == [v]
   where c = canonicalizeList . flattenOnion
-
-{-# DEPRECATED recurseOnion, removeType, firstOfType, equalOnion "These functions are outdated" #-}
-recurseOnion :: Expr -> [Expr]
-recurseOnion (Onion e1 e2) = recurseOnion e1 ++ recurseOnion e2
-recurseOnion x = [x]
-
-removeType :: Expr -> [Expr] -> [Expr]
-removeType _ [] = []
-removeType e@(Var _) ((Var _):xs) = removeType e xs
-removeType e@(Label _ _) ((Label _ _):xs) = removeType e xs
-removeType e@(Func _ _) ((Func _ _):xs) = removeType e xs
-removeType e@(Appl _ _) ((Appl _ _):xs) = removeType e xs
-removeType e@(PrimInt _) ((PrimInt _):xs) = removeType e xs
-removeType e@(PrimChar _) ((PrimChar _):xs) = removeType e xs
-removeType e@PrimUnit (PrimUnit:xs) = removeType e xs
-removeType e@(Case _ _) ((Case _ _):xs) = removeType e xs
-removeType _ ((Onion _ _):_) = error "Internal state error"
-removeType e (x:xs) = x : removeType e xs
-
-firstOfType :: Expr -> [Expr] -> Maybe Expr
-firstOfType _ [] = Nothing
-firstOfType (Var _) (x@(Var _):_) = Just x
-firstOfType (Label _ _) (x@(Label _ _):_) = Just x
-firstOfType (Func _ _) (x@(Func _ _):_) = Just x
-firstOfType (Appl _ _) (x@(Appl _ _):_) = Just x
-firstOfType (PrimInt _) (x@(PrimInt _):_) = Just x
-firstOfType (PrimChar _) (x@(PrimChar _):_) = Just x
-firstOfType PrimUnit (x@PrimUnit:_) = Just x
-firstOfType (Case _ _) (x@(Case _ _):_) = Just x
-firstOfType _ ((Onion _ _):_) = error "Internal state error"
-firstOfType e (_:xs) = firstOfType e xs
-
-equalOnion :: [Expr] -> [Expr] -> Bool
-equalOnion [] [] = True
-equalOnion [] _ = False
-equalOnion _ [] = False
-equalOnion (x:xs) ys = let ys' = removeType x ys
-                           xs' = removeType x xs
-                           v = firstOfType x ys
-                       in case v of
-                         Nothing -> False
-                         Just y -> ((x == y) && equalOnion xs' ys')
 
 -- |Evaluates a binary expression.
 evalBinop :: Expr              -- ^The first argument to the binary operator.
@@ -380,21 +383,21 @@ subst :: Expr    -- ^ The subexpression
       -> Expr    -- ^ The resulting expression
 
 subst v x e@(Var i)
-    | i == x      = v
-    | otherwise   = e
+  | i == x      = v
+  | otherwise   = e
 
 subst v x (Label name expr) =
-    Label name $ subst v x expr
+  Label name $ subst v x expr
 
 subst v x (Onion e1 e2) =
-    Onion (subst v x e1) (subst v x e2)
+  Onion (subst v x e1) (subst v x e2)
 
 subst v x e@(Func i body)
-    | i == x      = e
-    | otherwise   = Func i $ subst v x body
+  | i == x      = e
+  | otherwise   = Func i $ subst v x body
 
 subst v x (Appl e1 e2) =
-    Appl (subst v x e1) (subst v x e2)
+  Appl (subst v x e1) (subst v x e2)
 
 subst _ _ e@(PrimInt _) = e
 
@@ -403,23 +406,26 @@ subst _ _ e@(PrimChar _) = e
 subst _ _ e@(PrimUnit) = e
 
 subst v x (Case expr branches) =
-    let expr' = subst v x expr in
-    Case expr' $ map substBranch branches
-    where substBranch branch@(Branch mident chi branchExpr) =
-            let boundIdents =
-                    maybeToList mident ++
-                    case chi of
-                        ChiPrim _ -> []
-                        ChiLabel _ i -> [i]
-                        ChiFun -> []
-                        ChiAny -> []
-            in
-            if elem x boundIdents
-                then branch
-                else Branch mident chi $ subst v x branchExpr
+  let expr' = subst v x expr in
+  Case expr' $ map substBranch branches
+  where substBranch branch@(Branch mident chi branchExpr) =
+          let boundIdents =
+                maybeToList mident ++
+                case chi of
+                  ChiPrim _ -> []
+                  ChiLabel _ i -> [i]
+                  ChiFun -> []
+                  ChiAny -> []
+          in
+          if elem x boundIdents
+              then branch
+              else Branch mident chi $ subst v x branchExpr
+
+subst v x (OnionSub e s) =
+  OnionSub (subst v x e) s
 
 subst v x (LazyOp e1 op e2) =
-    LazyOp (subst v x e1) op (subst v x e2)
+  LazyOp (subst v x e1) op (subst v x e2)
 
 subst v x (EagerOp e1 op e2) =
-    EagerOp (subst v x e1) op (subst v x e2)
+  EagerOp (subst v x e1) op (subst v x e2)
