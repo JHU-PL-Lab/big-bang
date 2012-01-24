6404ee589d4aa394dae1447f8bdbfa11d80a2f7a
Alex Rozenshteyn <rpglover64@gmail.com>
2012-01-24 10:38:43 -0500

    Added AST nodes relevant to onion subtraction and adjust indentation.

--- src/Language/TinyBang/Ast.hs
+++ src/Language/TinyBang/Ast.hs
@@ -7,6 +7,7 @@ module Language.TinyBang.Ast
 -- Re-exported for convenience
 , LazyOperator(..)
 , EagerOperator(..)
+, Sigma(..)
 , exprFromValue
 ) where
 
@@ -24,42 +25,51 @@ import Language.TinyBang.Types.UtilTypes
 -------------------------------------------------------------------------------
 
 -- |Data type for representing Big Bang ASTs.
-data Expr =
-      Var Ident
-    | Label LabelName Expr
-    | Onion Expr Expr
-    | Func Ident Expr
-    | Appl Expr Expr
-    | PrimInt Integer
-    | PrimChar Char
-    | PrimUnit
-    | Case Expr Branches
-    | LazyOp Expr LazyOperator Expr
-    | EagerOp Expr EagerOperator Expr
-    deriving (Eq, Ord, Show)
+data Expr
+  = Var Ident
+  | Label LabelName Expr
+  | Onion Expr Expr
+  | OnionSub Expr Sigma
+  | Func Ident Expr
+  | Appl Expr Expr
+  | PrimInt Integer
+  | PrimChar Char
+  | PrimUnit
+  | Case Expr Branches
+  | LazyOp Expr LazyOperator Expr
+  | EagerOp Expr EagerOperator Expr
+  deriving (Eq, Ord, Show)
 
 -- |Data type for representing Big Bang values
-data Value =
-      VLabel LabelName Value
-    | VOnion Value Value
-    | VFunc Ident Expr
-    | VPrimInt Integer
-    | VPrimChar Char
-    | VPrimUnit
-    deriving (Eq, Ord, Show)
+data Value
+  = VLabel LabelName Value
+  | VOnion Value Value
+  | VFunc Ident Expr
+  | VPrimInt Integer
+  | VPrimChar Char
+  | VPrimUnit
+  | VEmptyOnion
+  deriving (Eq, Ord, Show)
 
 -- |Data type describing type patterns for case expressions.
-data Chi =
-      ChiPrim (T.PrimitiveType)
-    | ChiLabel LabelName Ident
-    | ChiFun
-    | ChiAny
-    deriving (Eq, Ord, Show)
+data Chi
+  = ChiPrim T.PrimitiveType
+  | ChiLabel LabelName Ident
+  | ChiFun
+  | ChiAny
+  deriving (Eq, Ord, Show)
+
+-- |Data type describing type patterns for removing values from onions.
+data Sigma
+  = SubPrim T.PrimitiveType
+  | SubLabel LabelName
+  | SubFunc
+  deriving (Eq, Ord, Show)
 
 -- |Alias for case branches
 type Branches = [Branch]
 data Branch = Branch (Maybe Ident) Chi Expr
-    deriving (Eq, Ord, Show)
+  deriving (Eq, Ord, Show)
 
 -- |Trivial conversion from values to exprs
 exprFromValue :: Value -> Expr
@@ -70,33 +80,44 @@ exprFromValue v = case v of
   VPrimInt i   -> PrimInt i
   VPrimChar c  -> PrimChar c
   VPrimUnit    -> PrimUnit
+  VEmptyOnion  -> OnionSub PrimUnit $ SubPrim T.PrimUnit
+
+instance Display Sigma where
+  makeDoc a = text "-" <> case a of
+    SubPrim p -> makeDoc p
+    SubLabel n -> makeDoc n
+    SubFunc -> text "fun"
 
 instance Display Expr where
-    makeDoc a = case a of
-        Var i -> text $ unIdent i
-        Label n e -> char '`' <> (text $ unLabelName n) <+> makeDoc e
-        Onion e1 e2 -> makeDoc e1 <+> char '&' <+> makeDoc e2
-        Func i e -> parens $
-                text "fun" <+> (text $ unIdent i) <+> text "->" <+> makeDoc e
-        Appl e1 e2 -> makeDoc e1 <+> makeDoc e2
-        PrimInt i -> integer i
-        PrimChar c -> quotes $ char c
-        PrimUnit -> parens empty
-        Case e brs -> text "case" <+> makeDoc e <+> text "of" <+> text "{" $+$
-                (nest indentSize $ vcat $ punctuate semi $ map makeDoc brs)
-                $+$ text "}"
-        LazyOp e1 op e2 -> makeDoc e1 <+> makeDoc op <+> makeDoc e2
-        EagerOp e1 op e2 -> makeDoc e1 <+> makeDoc op <+> makeDoc e2
-        {-
-           TODO: deal with the fact that the following isn't actually code
-           options include:
-               * errors on ASTs containing builtin nodes
-               * namespacing trick (e.g., Plus translates to
-                   "Language.TinyBang.Builtins.([+]) e1 e2"
-        -}
+  makeDoc a = case a of
+    Var i -> text $ unIdent i
+    Label n e -> char '`' <> (text $ unLabelName n) <+> makeDoc e
+    Onion e1 e2 -> makeDoc e1 <+> char '&' <+> makeDoc e2
+    Func i e -> parens $
+            text "fun" <+> (text $ unIdent i) <+> text "->" <+> makeDoc e
+    Appl e1 e2 -> makeDoc e1 <+> makeDoc e2
+    PrimInt i -> integer i
+    PrimChar c -> quotes $ char c
+    PrimUnit -> parens empty
+    Case e brs -> text "case" <+> makeDoc e <+> text "of" <+> text "{" $+$
+            (nest indentSize $ vcat $ punctuate semi $ map makeDoc brs)
+            $+$ text "}"
+    OnionSub e s -> makeDoc e <+> char '&' <> makeDoc s
+    LazyOp e1 op e2 -> makeDoc e1 <+> makeDoc op <+> makeDoc e2
+    EagerOp e1 op e2 -> makeDoc e1 <+> makeDoc op <+> makeDoc e2
+    {-
+       TODO: deal with the fact that the following isn't actually code
+       options include:
+           * errors on ASTs containing builtin nodes
+           * namespacing trick (e.g., Plus translates to
+               "Language.TinyBang.Builtins.([+]) e1 e2"
+    -}
 
 instance Display Value where
-  makeDoc = makeDoc . exprFromValue
+  makeDoc x =
+    case x of
+      VEmptyOnion -> text "(&)"
+      _ -> makeDoc $ exprFromValue x
 
 instance Display Branch where
     makeDoc (Branch mident chi e) =
