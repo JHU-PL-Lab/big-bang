4af9b259f6f50d15c9d00e492a861c0e32717dda
Alex Rozenshteyn <rpglover64@gmail.com>
2012-01-25 19:52:59 -0500

    Added some tests for binders.

--- executables/tests/Test.hs
+++ executables/tests/Test.hs
@@ -208,8 +208,7 @@ tests = TestList $ [TPP.tests] ++
   , xPars "def x = 3 in x = 4 in x" $
           A.Def idX (A.PrimInt 3) $ A.Assign (A.AIdent idX) efour varX
 -- Test evaluation of definition and assignment
-  , xEval "def x = 4 in x" $
-          four
+  , xEval "def x = 4 in x" four
   , xNotC "x = 4 in x"
   , xEval "def x = 3 in x = 4 in x" four
   , xEval "def x = () in x = 4 in x" four
@@ -217,6 +216,11 @@ tests = TestList $ [TPP.tests] ++
   , xCont "def x = () in x = 2 in case x of { unit -> 4 }"
   , xCont "def x = () in x = 2 in case x of { int -> 4 }"
   , xEval "def x = () in x = 2 in case x of { unit -> 2 ; int -> 4 }" four
+-- Test binders
+  , xEval "case () & 0 of {x:int -> x}" zero
+  , xEval "case () & 0 of {x:unit -> x}" A.VPrimUnit
+  , xEval "case () & 0 of {x:_ -> x}" $
+          A.VOnion (A.VPrimInt 0) A.VPrimUnit
 -- Test proper handling of arbitrary ASCII characters
   , xType "'x'"
   , xEval "'a'" $
