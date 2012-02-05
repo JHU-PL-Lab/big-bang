ef0b42e76e3e1f2945fdf591d3f96fa71be93ee7
Alex Rozenshteyn <rpglover64@gmail.com>
2012-02-05 18:16:52 -0500

    Added more different ways to write sum function.

--- tests/lib-src/Language/TinyBang/Test/ListEncoding.hs
+++ tests/lib-src/Language/TinyBang/Test/ListEncoding.hs
@@ -13,15 +13,49 @@ srcMakeList = foldr addNode "`nil ()"
   where addNode int tbCode =
           printf "`hd %d & (`tl (%s))" int tbCode
 
-srcSum = "fun this -> fun xs ->"
-         ++ " case xs of "
-         ++ " { `nil junk -> 0 ;"
-         ++ "   `hd a -> case xs of"
-         ++ "       {`tl b -> [+] a (this b)}"
-         ++ " }"
+srcSum1 = "fun this -> fun xs ->"
+          ++ " case xs of"
+          ++ " { `nil junk -> 0 ;"
+          ++ "   `hd a -> case xs of"
+          ++ "       {`tl b -> [+] a (this b)}"
+          ++ " }"
+
+srcSum2 = "fun this -> fun accum -> fun xs ->"
+          ++ " case xs of"
+          ++ " { `nil junk -> accum ;"
+          ++ "   `hd a -> case xs of"
+          ++ "       {`tl b -> this ([+] a accum) b}"
+          ++ " }"
+
+srcSum3 = "fun this -> fun xs ->"
+          ++ " case xs of { `acc accum ->"
+          ++ " case xs of"
+          ++ " { `nil junk -> accum;"
+          ++ "   `hd a -> case xs of"
+          ++ "       {`tl b -> this (b & `acc ([+] a accum))}"
+          ++ " }}"
+
+srcSum4 = "def accum = 0 in fun this -> fun xs ->"
+          ++ " case xs of"
+          ++ " { `nil junk -> accum;"
+          ++ "   `hd a -> case xs of"
+          ++ "       {`tl b -> accum = [+] accum a in this b }"
+          ++ " }"
+
+testSum xs = map ($ V.pi $ sum xs)
+  [ xEval $ srcMultiAppl
+      [srcY, srcSum1, srcMakeList xs]
+  , xEval $ srcMultiAppl
+      [srcY, srcSum2, "0", srcMakeList xs]
+  , xEval $ srcMultiAppl
+      [srcY, srcSum3, "`acc 0 & " ++ srcMakeList xs]
+  , xEval $ srcMultiAppl
+      [srcY, srcSum4, srcMakeList xs]
+  ]
 
 tests :: (?debug :: Bool) => Test
-tests = TestLabel "List encoding tests" $ TestList
-  [ xEval (srcMultiAppl [srcY, srcSum, srcMakeList [1,2,3]]) $
-          V.pi 6
+tests = TestLabel "List encoding tests" $ TestList $ concat
+  [ testSum []
+  , testSum [1,2,3]
+  , testSum [3,2,1]
   ]
