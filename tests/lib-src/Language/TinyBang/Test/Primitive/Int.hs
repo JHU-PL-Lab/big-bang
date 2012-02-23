6727d3fcc52af75e1c97f99578f16e2a67abf318
Zachary Palmer <zep_git@bahj.com>
2012-02-23 10:54:18 -0500

    Fixed parsing expectations in integer tests.

--- tests/lib-src/Language/TinyBang/Test/Primitive/Int.hs
+++ tests/lib-src/Language/TinyBang/Test/Primitive/Int.hs
@@ -20,7 +20,7 @@ tests = TestLabel "Integer tests" $ TestList $
   , testInteger (-1234567890)
   ] ++ map testInteger [-50, 50] ++
   [ fPars "1_1"
-  , fPars "_1"
+  , xPars "_1" (A.Var $ ident "_1")
   , fPars "1_"
   , xLexs "0x1"
       [TokIntegerLiteral 0, TokIdentifier "x1"]
