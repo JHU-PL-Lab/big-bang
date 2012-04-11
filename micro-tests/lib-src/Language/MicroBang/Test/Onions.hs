0581c29e312cc116814b453ded98c9310986b163
Leah Hanson <astrieanna@gmail.com>
2012-04-11 12:28:04 -0400

    commented out some Onion tests that used def .. in

--- micro-tests/lib-src/Language/MicroBang/Test/Onions.hs
+++ micro-tests/lib-src/Language/MicroBang/Test/Onions.hs
@@ -49,20 +49,20 @@ tests = TestLabel "Tests of basic onion properties" $ TestList
                    (A.Onion (A.Label lblB $ E.pi 2) (A.Label lblC $ E.pi 3)))
   -- Test that onions evaluate left to right
   -- Uses state
-  , xEval " def x = 0 in \
-          \ `A (x = [+] x 1 in x) & `B (x = [+] x 1 in x)"
-          ( A.VOnion (A.VLabel lblA $ A.VPrimInt 0) (A.VLabel lblB $ A.VPrimInt 1))
-  , xEval " def x = `Ref 0 in \
-          \ def inc = fun x -> case x of { `Ref y -> y = [+] y 1 in y } in \
-          \ `A (inc x) & `B (inc x)"
-          ( A.VOnion (A.VLabel lblA $ A.VPrimInt 0) (A.VLabel lblB $ A.VPrimInt 1)
-          , mkState [ (0, V.pi 1)
-                    , (1, V.pi 2)
-                    ]
-          )
-  , xEval " def r = `Ref 0 in \
-          \ def inc = fun x -> case x of { `Ref y -> y = [+] y 1 in y } in \
-          \ `A (case r of { `Ref x -> x = [+] x 1 in x }) & \
-          \ `B (case r of { `Ref x -> x = [+] x 1 in x })"
-          ( A.VOnion (A.VLabel lblA $ A.VPrimInt 0) (A.VLabel lblB $ A.VPrimInt 1))
+  --, xEval " def x = 0 in \
+  --        \ `A (x = [+] x 1 in x) & `B (x = [+] x 1 in x)"
+  --        ( A.VOnion (A.VLabel lblA $ A.VPrimInt 0) (A.VLabel lblB $ A.VPrimInt 1))
+  --, xEval " def x = `Ref 0 in \
+  --        \ def inc = fun x -> case x of { `Ref y -> y = [+] y 1 in y } in \
+  --        \ `A (inc x) & `B (inc x)"
+  --        ( A.VOnion (A.VLabel lblA $ A.VPrimInt 0) (A.VLabel lblB $ A.VPrimInt 1)
+  --        , mkState [ (0, V.pi 1)
+  --                  , (1, V.pi 2)
+  --                  ]
+  --        )
+  --, xEval " def r = `Ref 0 in \
+  --        \ def inc = fun x -> case x of { `Ref y -> y = [+] y 1 in y } in \
+  --        \ `A (case r of { `Ref x -> x = [+] x 1 in x }) & \
+  --        \ `B (case r of { `Ref x -> x = [+] x 1 in x })"
+  --        ( A.VOnion (A.VLabel lblA $ A.VPrimInt 0) (A.VLabel lblB $ A.VPrimInt 1))
   ]
