d9c8ec9b3be1d6ef2d42456f88963687d3344644
Leah Hanson <astrieanna@gmail.com>
2012-04-11 22:46:11 -0400

    fixed some and removed unused tests from LazyOps

--- micro-tests/lib-src/Language/MicroBang/Test/LazyOps.hs
+++ micro-tests/lib-src/Language/MicroBang/Test/LazyOps.hs
@@ -17,46 +17,25 @@ import qualified Language.MicroBang.Ast as A
 tests :: (?debug :: Bool) => Test
 tests = TestLabel "Tests on integer operations" $ TestList
   [
---  lexParseEval "[+] ([-] 3 1) 2"
---                 [ TokOpPlus
---                 , TokOpenParen
---                 , TokOpMinus
---                 , TokIntegerLiteral 3
---                 , TokIntegerLiteral 1
---                 , TokCloseParen
---                 , TokIntegerLiteral 2
---                 ]
---                 (A.LazyOp A.Plus (A.LazyOp A.Minus (E.pi 3) (E.pi 1)) (E.pi 2))
---                 (V.pi 4)
+  lexParseEval "[+] ([+] 3 1) 2"
+                 (A.BinOp A.Plus (A.BinOp A.Plus (E.pi 3) (E.pi 1)) (E.pi 2))
+                 (V.pi 6)
 ---- Test parse and evaluation of some simple arithmetic applications
---  , xPars "[+] 2 2" $
---          A.LazyOp A.Plus (E.pi 2) (E.pi 2)
---  , xType "[+] 1 2"
---  , xType "[-] 1 2"
---  , xType "[+] ([-] ([+] 1 2) 3) ([+] (-2) ([-] 4 0))"
-  --,
-    xEval "(fun x -> [+] x x) 2"
+  , xPars "[+] 2 2" $
+          A.BinOp A.Plus (E.pi 2) (E.pi 2)
+  , xEval "(fun x -> [+] x x) 2"
           (V.pi 4)
   , xEval "(fun x -> [+] x x) 2"
           (V.pi 4)
   , xEval "[+] 2 2"
           (V.pi 4)
-  --, xEval "[-] 2 2"
-  --        (V.pi 0)
-  --, xEval "[-] 2 -2"
-  --        (V.pi 4)
---  , xType "(fun x -> [+] x 1) 1"
+  , xEval "[+] 2 -2"
+          (V.pi 0)
 ---- Test that arithmetic expressions on non-numeric literals fail to typecheck
---  , xCont "[+] 1 'a'"
---  , xCont "[+] 1 ()"
---  , xCont "[+] 'a' 'a'"
---  , xCont "[+] () ()"
---  , xCont "[+] 2 'x'"
---  , xCont "[+] 1 (fun x -> x)"
---  , xCont "[-] 1 'a'"
---  , xCont "[-] 1 ()"
---  , xCont "[-] 'a' 'a'"
---  , xCont "[-] () ()"
+  , xCont "[+] 1 ()"
+  , xCont "[+] () ()"
+  , xCont "[+] 2  `A 3"
+  , xCont "[+] 1 (fun x -> x)"
 -- Test evaluation of compound arithmetic application
   , xEval "[+] ([+] 1 -1) ([+] 1 -1)"
           (V.pi 0)
