a37a0118d8e07049e02118a31e5e529919588cdc
Zachary Palmer <zep_git@bahj.com>
2012-04-23 13:04:37 -0400

    Added a unit test.

--- little-tests/lib-src/Language/LittleBang/Test/Self.hs
+++ little-tests/lib-src/Language/LittleBang/Test/Self.hs
@@ -3,6 +3,7 @@ module Language.LittleBang.Test.Self
 )
 where
 
+import Language.LittleBang.Test.NameUtils
 import qualified Language.LittleBang.Test.ValueUtils as V
 import Language.LittleBang.Test.UtilFunctions
 
@@ -10,7 +11,11 @@ import qualified Language.LittleBang.Ast as LA
 import qualified Language.TinyBang.Ast as TA
 import qualified Language.TinyBang.Config as Cfg
 
+import qualified Data.IntMap as IntMap
+
 -- TODO: Write quickcheck properties that the empty onion is the left and right identity.
+
+mkState = IntMap.fromList
 
 tests :: (?conf :: Cfg.Config) => Test
 tests = TestLabel "Tests of self" $ TestList
@@ -20,4 +25,14 @@ tests = TestLabel "Tests of self" $ TestList
           \              }) in                                          \
           \x.Sum 5                                                      "
           $ V.pi 15
+  , xEval "def evenOdd = `isEven (fun x -> case x of {                  \
+          \                                  `S y -> self.isOdd y;      \
+          \                                  `Z _ -> `True ()}) &       \
+          \              `isOdd (fun x -> case x of {                   \
+          \                                 `S y -> self.isEven y;      \
+          \                                 `Z _ -> `False ()})         \
+          \in evenOdd.isEven (`S `S `S `S `S `Z ())                     "
+          $ ( TA.VLabel tlblFalse 0
+            , mkState [ (0, TA.VPrimUnit) ]
+            )
   ]
