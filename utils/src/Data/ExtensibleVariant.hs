9225d4c264710fede89175de9d8f65e51247b855
Zachary Palmer <zep_git@bahj.com>
2012-07-06 14:45:37 -0400

    Changed generated AST structure names to more general extensible variant names.

---
+++ utils/src/Data/ExtensibleVariant.hs
@@ -0,0 +1,145 @@
+{-# LANGUAGE      TemplateHaskell
+                , MultiParamTypeClasses
+                , FlexibleInstances
+                , FlexibleContexts
+                , UndecidableInstances
+                , ScopedTypeVariables
+                , TypeOperators
+                #-}
+
+{-|
+  This module provides an extensible variant datatype.  This datatype is
+  similar in some behavior to the coproduct-with-subsumption type that is
+  defined in Data Types a la Carte: it permits a user to create a variant
+  data type, specifically by naming a series of data types of kind * -> *
+  which expect the recursive type as their argument.  For instance, a variant
+  could be constructed as follows:
+
+  @
+    data Lit a = IntL Integer | UnitL
+    data Arith a = Plus a a | Neg a
+    type MathAst = Lit :|: Arith
+    x :: MathAst = inj $ Neg $ inj $ Plus (inj $ IntL 2) (inj $ IntL 3)
+  @
+
+  For better legibility, smart constructors can be defined:
+
+  @
+    plus :: (Plus :<< t) => t -> t -> t
+    plus x y = inj $ Plus x y
+    ...
+    x = neg $ plus (intL 2) (intL 3)
+  @
+
+  TODO: more examples, especially w.r.t. operations
+
+  While somewhat similar as a fundamental data structure, the implementation
+  is considerably different.  Instead of using an incremental construction
+  model with subsumption, extensible variant types are flat and have fixed
+  arity.  This is somewhat restrictive in that it imposes a maximum width for
+  extensible variant width, but it also leads to somewhat simpler code which is
+  more amenable to type inference.
+
+  TODO: more docs
+-}
+
+module Data.ExtensibleVariant where
+
+import Control.Monad.Identity
+import qualified Data.ExtensibleVariant.Meta as Meta
+import Utils.Render.Display
+
+$( return $ concat $ map Meta.xvDecls Meta.xvArities )
+
+-- |Defines operations over an extensible variant structure.  Instances of this
+--  typeclass are provided by this module; they invoke the @XvPart@ instances of
+--  the variants' members.  The @op@ argument is meant to provide a means by
+--  which different typeclass instances can be disambiguated; it is passed
+--  verbatim to the @XvPart@ typeclass instance.
+class XvOp op xv result where
+  xvop :: op -> xv -> result
+
+-- |Performs an operation over a component of an extensible variant structure.
+--  Users of this module are expected to provide instances of this typeclass to
+--  define AST operations.  Selection of the specific @XvPart@ instance is
+--  typically driven by the @op@ argument.
+class XvPart op comp xv result where
+  xvpart :: op -> comp xv -> result
+
+$( return $ concat $ map Meta.opDecls Meta.xvArities )
+
+-- |Indicates containment of a component in an extensible variant type.  This
+--  module provides implementations of this typeclass for each concrete
+--  extensible variant type.
+class comp :<< xv where
+  -- Injects a provided component into an extensible variant structure.
+  inj :: comp xv -> xv
+  -- Projects a given component type from an extensible variant structure.  If
+  -- that component is not the top level of the structure, Nothing is returned.
+  prj :: xv -> Maybe (comp xv)
+
+$( return $ concat $ map (\(i,j) -> Meta.containDecls i j)
+    [(i,j) | j <- Meta.xvArities, i <- [1..j] ] )
+
+-- |Defines a monadic homomorphic operation over extensible variant component
+--  types.  Users of this module should provide an instance of this typeclass
+--  when the component is to be used in transformations relying on homomorphisms
+--  (such as upcasting).  Homomorphism instances should be of the form
+--  @
+--      instance (XvWrap comp, Monad m)
+--            => XvPart HomOpM comp xv1 ((xv1 -> m xv2) -> m xv2)
+--  @
+--  The function of type @xv1 -> m xv2@ is to be used on each child in the
+--  component.
+data HomOpM = HomOpM
+
+-- |Defines a non-monadic homomorphic operation over extensible variant
+--  component types.  This operation is provided for convenience; it uses the
+--  Identity monad.  This module provides the appropriate typeclass instance
+--  for any type for which @HomOpM@ has been defined.
+data HomOp = HomOp
+
+-- |A typeclass instance for @HomOp@ given @HomOpM@.
+instance (XvPart HomOpM comp xv1 ((xv1 -> Identity xv2) -> Identity xv2))
+      => XvPart HomOp comp xv1 ((xv1 -> xv2) -> xv2) where
+  xvpart HomOp comp = \f ->
+    runIdentity $ xvpart HomOpM comp ((return . f)::(xv1 -> Identity xv2))
+
+-- |Performs an upcasting operation on an extensible variant over which
+--  homomorphisms are defined.
+--  NOTE: due to a bug present in at least GHC 7.4.1, the GHC compiler will
+--        suggest an incorrect type signature for this function.  The type
+--        signature shown here, in concert with the explicit annotation of
+--        upcast in the definition, is sufficient.   The ticket for this bug
+--        can be found at http://hackage.haskell.org/trac/ghc/ticket/6065
+upcast :: forall xv1 xv2.
+            (XvOp HomOp xv1 ((xv1 -> xv2) -> xv2)) => xv1 -> xv2
+upcast xv = xvop HomOp xv (upcast :: xv1 -> xv2)
+
+-- |Defines a monadic catamorphic operation over extensible variant component
+--  types which produces a monoidal result.  Users of this module may provide
+--  instances of this typeclass for generalization of catamorphic operations
+--  (such as free variable assessment) and then define specific catamorphisms
+--  by defining cases for exceptions and then invoking the catamorphism as
+--  necessary.  Catamorphism instances should be of the form
+--  @
+--      instance (Monoid r, Monad m)
+--            => XvPart CatOpM comp xv ((xv -> m r) -> m r)
+--  @
+--  The function of type @xv -> m r@ is to be used on each child node in the
+--  component type; results are to be combined using @mappend@ and base
+--  cases should be defined using @mempty@.
+data CatOpM = CatOpM
+
+-- |Defines a non-monadic catamorphic operation returning a monoid.  This
+--  operation is provided for convenience; it uses the Identity monad.  This
+--  module provides the appropriate typeclass instance for any type for which
+--  @CatOpM@ has been defined.
+data CatOp = CatOp
+
+-- |A typeclass instance for @CatOp@ given @CatOpM@.
+instance (XvPart CatOpM comp ast ((ast -> Identity r) -> Identity r))
+      => XvPart CatOp comp ast ((ast -> r) -> r) where
+  xvpart CatOp comp f =
+    runIdentity $ xvpart CatOpM comp ((return . f)::(ast -> Identity r))
+
---
+++ utils/src/Data/ExtensibleVariant/Meta.hs
@@ -0,0 +1,182 @@
+{-# LANGUAGE      TemplateHaskell
+                #-}
+
+{-|
+  Metaprogramming facilities used by the Data.ExtensibleVariant module.
+-}
+module Data.ExtensibleVariant.Meta
+( xvArities
+, xvDecls
+, opDecls
+, containDecls
+) where
+
+import Language.Haskell.TH.Syntax
+
+-- |The arities for which variant types are defined.
+xvArities :: [Int]
+xvArities = [1..4]
+
+-- |A function to produce declarations for the k-ary variant type.
+xvDecls :: Int -> [Dec]
+xvDecls n = [declXvType n, declXvEq n, declXvOrd n, declXvShow n
+            ,declXvDisplay n]
+
+-- |A function to produce the @XvOp@ declarations for the k-ary variant type.
+opDecls :: Int -> [Dec]
+opDecls n = [declXvOp n]
+
+-- |A function to produce the @:<<@ declarations for the ith position of
+--  the j-ary variant type.
+containDecls :: Int -> Int -> [Dec]
+containDecls i j = [declXvContain i j]
+
+-- |Creates the declaration for the k-ary variant type itself.
+declXvType :: Int -> Dec
+declXvType n =
+  DataD [] name tvbinds constrs []
+  where name = xvName n
+        tvnames = xvTvnames n
+        tvbinds = map PlainTV tvnames
+        makeConstr i =
+          NormalC (xvConstrName n i) $
+            [(NotStrict,AppT (VarT $ tvnames !! (i-1)) $
+              foldl AppT (ConT name) (map VarT tvnames))]
+        constrs = map makeConstr [1..n]
+
+-- |Creates a typeclass instance declaration of one method for a k-ary variant
+--  type.
+declXvTypeclassInstance :: String -> String -> [Clause] -> Int -> Dec
+declXvTypeclassInstance scname sfname clauses n =
+  InstanceD preds typ [impl]
+  where name = xvName n
+        tvnames = xvTvnames n
+        preds = map makePred [1..n]
+        cname = mkName scname
+        fname = mkName sfname
+        makePred i = ClassP cname $
+            [AppT (VarT $ tvnames !! (i-1)) $ foldl AppT (ConT name) $
+                map VarT tvnames]
+        typ = AppT (ConT cname) $ foldl AppT (ConT name) $ map VarT tvnames
+        impl = FunD fname clauses
+ 
+-- |Creates the @Eq@ typeclass instance declaration for the k-ary variant type.
+declXvEq :: Int -> Dec
+declXvEq n = declXvTypeclassInstance "Eq" "==" clauses n
+  where clauses = (map makeClause [1..n]) ++
+                  (if n == 1 then [] else [defaultClause])
+        makeClause i =
+            Clause [ConP constrName $ [VarP x], ConP constrName $ [VarP y]]
+            (NormalB $ AppE (AppE (VarE $ mkName "==") (VarE x)) (VarE y))
+            []
+          where x = mkName "x"
+                y = mkName "y"
+                constrName = xvConstrName n i
+        defaultClause =
+            Clause [WildP,WildP] (NormalB $ ConE $ mkName "False") []
+
+-- |Creates the @Ord@ typeclass instance declaration for the k-ary variant type.
+declXvOrd :: Int -> Dec
+declXvOrd n = declXvTypeclassInstance "Ord" "compare" clauses n
+  where clauses = map makeClause [(a,b) | a <- [1..n], b <- [1..n]]
+        makeClause (i,j) =
+            case compare i j of
+              EQ -> eqClause
+              LT -> conClause "LT"
+              GT -> conClause "GT"
+          where x = mkName "x"
+                y = mkName "y"
+                constrNameI = xvConstrName n i
+                constrNameJ = xvConstrName n j
+                eqClause =
+                  Clause [ConP constrNameI $ [VarP x]
+                         ,ConP constrNameJ $ [VarP y]]
+                    (NormalB $ AppE (AppE (VarE $ mkName "compare")
+                                 (VarE x)) (VarE y)) []
+                conClause nm =
+                  Clause [ConP constrNameI $ [WildP]
+                         ,ConP constrNameJ $ [WildP]]
+                    (NormalB $ ConE $ mkName nm) []
+
+-- |Creates the @Show@ typeclass instance declaration for the k-ary variant type.
+declXvShow :: Int -> Dec
+declXvShow n = declXvTypeclassInstance "Show" "show" clauses n
+  where clauses = map makeClause [1..n]
+        makeClause i =
+            Clause [ConP constrName $ [VarP x]]
+            (NormalB $ AppE (AppE (VarE $ mkName "++")
+                        (AppE
+                            (AppE (VarE $ mkName "++") $
+                                LitE $ StringL $ showName constrName ++ " (")
+                            (AppE (VarE $ mkName "show") $ VarE x)
+                        )) $ LitE $ StringL $ ")")
+            []
+          where x = mkName "x"
+                constrName = xvConstrName n i
+
+-- |Creates the @Display@ typeclass instance declaration for the k-ary variant
+--  type.
+declXvDisplay :: Int -> Dec
+declXvDisplay n = declXvTypeclassInstance "Display" "makeDoc" clauses n
+  where clauses = map makeClause [1..n]
+        makeClause i =
+            Clause [ConP constrName $ [VarP x]]
+            (NormalB $ AppE (VarE $ mkName "makeDoc") $ VarE x) []
+          where x = mkName "x"
+                constrName = xvConstrName n i
+
+-- |Creates the @XvOp@ typeclass instance declaration for the k-ary variant type.
+declXvOp :: Int -> Dec
+declXvOp n =
+  InstanceD preds typ [impl]
+  where name = xvName n
+        tvnames = xvTvnames n
+        preds = map makePred [1..n]
+        result = mkName "result"
+        op = mkName "op"
+        makePred i = ClassP (mkName "XvPart") $
+            [ VarT $ op
+            , VarT $ tvnames !! (i-1)
+            , foldl AppT (ConT name) $ map VarT tvnames
+            , VarT result ]
+        typ = AppT (AppT (AppT (ConT $ mkName "XvOp") $ VarT op) $
+                foldl AppT (ConT name) $
+                    map VarT tvnames) $ VarT result
+        xv = mkName "xv"
+        impl = FunD (mkName "xvop") $
+            [Clause [VarP op, VarP xv] (NormalB $ CaseE (VarE xv) matches) []]
+        matches = map makeMatch [1..n]
+        p = mkName "p"
+        makeMatch i = Match (ConP (xvConstrName n i) [VarP p])
+                        (NormalB $ AppE (AppE (VarE $ mkName "xvpart") $
+                            VarE op) $ VarE p) []
+
+-- |Creates the @:<<@ typeclass instance declarations for the k-ary variant
+--  type.
+declXvContain :: Int -> Int -> Dec
+declXvContain i j =
+  InstanceD [] typ [inj,prj]
+  where name = xvName j
+        tvnames = xvTvnames j
+        typ = AppT (AppT (ConT $ mkName ":<<") $ VarT $ tvnames !! (i-1)) $
+                foldl AppT (ConT name) $ map VarT tvnames
+        inj = FunD (mkName "inj") $
+            [Clause [] (NormalB $ ConE $ xvConstrName j i) []]
+        prjClauseGood = Clause [ConP (xvConstrName j i) [VarP $ mkName "x"]]
+                (NormalB $ AppE (ConE $ mkName "Just") $ VarE $ mkName "x") []
+        prjClauseBad = Clause [WildP] (NormalB $ ConE $ mkName "Nothing") [] 
+        prj = FunD (mkName "prj") $
+            prjClauseGood : (if j > 1 then [prjClauseBad] else [])
+
+-- |Creates the name for the k-ary variant type.
+xvName :: Int -> Name
+xvName n = mkName $ "Xv" ++ show n
+
+-- |Creates type variable names for a k-ary variant type.
+xvTvnames :: Int -> [Name]
+xvTvnames n = map (mkName . ("p" ++) . show) [1..n]
+
+-- |Creates the ith constructor name for a k-ary variant type.
+xvConstrName :: Int -> Int -> Name
+xvConstrName n i = mkName $ "Xv" ++ show n ++ "p" ++ show i
+
