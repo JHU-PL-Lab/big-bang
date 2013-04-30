9f97f54a324a65cf3834a8ee766ad35be0fdc908
Zachary Palmer <zep_git@bahj.com>
2013-04-30 22:19:11 +0000

    Added ValueDsl glue function.

--- tiny-bang/test-src/Tests/ValueDsl.hs
+++ tiny-bang/test-src/Tests/ValueDsl.hs
@@ -1,4 +1,11 @@
 module Tests.ValueDsl
-(
+( parseValueDslPredicate
+, DeepOnionPredicate
 ) where
 
+import Tests.ValueDsl.Lexer
+import Tests.ValueDsl.Parser
+
+-- | A function to parse a value DSL string into a predicate function.
+parseValueDslPredicate :: String -> Either String DeepOnionPredicate
+parseValueDslPredicate str = parseValueDsl =<< lexValueDsl str
