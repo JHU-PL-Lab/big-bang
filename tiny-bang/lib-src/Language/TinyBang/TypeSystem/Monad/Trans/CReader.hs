8c097ea64b09262025a498f38921f1509efc257e
Alex Rozenshteyn <rpglover64@gmail.com>
2013-05-10 15:36:58 -0400

    Add constraints to CReader typeclass and eliminators.

--- tiny-bang/lib-src/Language/TinyBang/TypeSystem/Monad/Trans/CReader.hs
+++ tiny-bang/lib-src/Language/TinyBang/TypeSystem/Monad/Trans/CReader.hs
@@ -13,21 +13,23 @@ import Control.Monad.Identity (Identity, runIdentity)
 import Control.Monad.Trans (MonadTrans)
 import Control.Applicative (Applicative, Alternative)
 
+import Language.TinyBang.TypeSystem.ConstraintDatabase
+
 type CReader db a = CReaderT db Identity a
 
-runCReader :: CReader db a -> db -> a
+runCReader :: ConstraintDatabase db => CReader db a -> db -> a
 runCReader m = runIdentity . runCReaderT m
 
-runCReaderT :: CReaderT db m a -> db -> m a
+runCReaderT :: ConstraintDatabase db => CReaderT db m a -> db -> m a
 runCReaderT (CReaderT m) = runReaderT m
 
 newtype CReaderT db m a = CReaderT (ReaderT db m a)
   deriving (Monad, Functor, MonadTrans, Applicative, Alternative)
 
-class MonadCReader db m | m -> db where
-  askDB :: m db
-  localDB :: (db -> db) -> m a -> m a
+class ConstraintDatabase db => MonadCReader db m | m -> db where
+  askDb :: m db
+  localDb :: (db -> db) -> m a -> m a
 
-instance (Monad m) => MonadCReader db (CReaderT db m) where
-  askDB = CReaderT ask
-  localDB f (CReaderT m) = CReaderT $ local f m
+instance (Monad m, ConstraintDatabase db) => MonadCReader db (CReaderT db m) where
+  askDb = CReaderT ask
+  localDb f (CReaderT m) = CReaderT $ local f m
