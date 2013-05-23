f33675c9a71b55eda70c446dcea96ae0f0601f9b
Zachary Palmer <zep_git@bahj.com>
2013-05-23 11:22:06 -0400

    Expanded on NFA tests to include epsilon NFA generation.

--- tiny-bang/test-src/Test/Data/NFA.hs
+++ tiny-bang/test-src/Test/Data/NFA.hs
@@ -15,7 +15,7 @@ type TestNfa = Nfa Int Char
 example1 :: TestNfa
 example1 = createFromData 1 [(1,'a',2)] [2]
 
-{-@
+{-|@
   (1) --a--> ((2))
     \          ^
      \         |
@@ -24,7 +24,7 @@ example1 = createFromData 1 [(1,'a',2)] [2]
 example2 :: TestNfa
 example2 = createFromData 1 [(1,'a',2),(1,'b',3),(3,'c',2)] [2]
 
-{-@
+{-|@
   (1) --a--> ((2)) <---
                |       |
                 -b,c,d-
@@ -32,18 +32,35 @@ example2 = createFromData 1 [(1,'a',2),(1,'b',3),(3,'c',2)] [2]
 example3 :: TestNfa
 example3 = createFromData 1 ((1,'a',2):map (2,,2) "bcd") [2]
 
-{-@
+{-|@
   (1) --a--> 2 --c--> 3 --b--> 4 --d--> ((5))
 @-}
 example4 :: TestNfa
 example4 = createFromData 1 (map (\(x,e) -> (x,e,x+1)) $ zip [1..] "acbd") [5]
 
-{-@
+{-|@
   (1)   ((2))
 @-}
 example5 :: TestNfa
 example5 = createFromData 1 [] [2]
 
+{-|@
+  (1) ----> 2<-
+   |        |  |
+   |        b  |
+   |        | /
+   |        v/
+    --a---> 3 --c--> ((4))
+@-}
+example6 :: TestNfa
+example6 = createFromDataWithEpsilon 1
+              [ (1,Nothing,2)
+              , (1,Just 'a',3)
+              , (2,Just 'b',3)
+              , (3,Nothing,2)
+              , (3,Just 'c',4) ] [4]
+
+-- |The tests for this module.
 nfaTests :: Test
 nfaTests =
   let ex1 = ("example1",example1)
@@ -51,11 +68,15 @@ nfaTests =
       ex3 = ("example3",example3)
       ex4 = ("example4",example4)
       ex5 = ("example5",example5)
+      ex6 = ("example6",example6)
       ex1or2 = ("ex1|ex2", example1 `union` example2)
       ex1and2 = ("ex1&ex2", example1 `intersect` example2)
       ex2and3 = ("ex2&ex3", example2 `intersect` example3)
       ex1and4 = ("ex1&ex4", example1 `intersect` example4)
       ex3and4 = ("ex3&ex4", example3 `intersect` example4)
+      ex1and6 = ("ex1&ex6", example1 `intersect` example6)
+      ex2and6 = ("ex2&ex6", example2 `intersect` example6)
+      ex3and6 = ("ex3&ex6", example3 `intersect` example6)
   in
   TestList
     [ TestLabel "Simple acceptance" $ TestList $
@@ -70,12 +91,19 @@ nfaTests =
             [ "acbd"]
             [ "a", "ab", "ac", "ad", "abcd", "abb", "abcdb", "abdbcbbdb", "accc"
             , "", "aa", "b", "bb", "bcd", "bcda", "e" ]
+        ++  genContainTests ex6
+            [ "bc", "bbbc", "abc", "ac", "abbbc" ]
+            [ "c", "aac", "ab", "abbbb", "" ]
     , TestLabel "Combined acceptance" $ TestList $
             genContainTests ex1or2 ["a", "bc"] ["", "aa", "abc", "bca", "cb"]
         ++  genContainTests ex1and2 ["a"] ["bc", "aa", ""]
         ++  genContainTests ex2and3 ["a"] ["ad", "abc", "bc", "ac", "acb"]
         ++  genContainTests ex1and4 [] ["a", "acbd", "cbd", "aa"]
         ++  genContainTests ex3and4 ["acbd"] ["acbdd", "a", "bcdd", "abc"]
+        ++  genContainTests ex2and6 ["bc"] ["ac", "a", "abc"]
+        ++  genContainTests ex3and6
+            [ "abc", "abbbbc", "ac" ]
+            [ "bc", "bbbc", "abcd", "acc", "acb", "" ]
     , TestLabel "Emptiness" $ TestList $
             map (emptinessTest False) [ex1,ex2,ex3,ex4]
         ++  [ emptinessTest True ex5
@@ -84,6 +112,9 @@ nfaTests =
             , emptinessTest False ex2and3
             , emptinessTest True ex1and4
             , emptinessTest False ex3and4
+            , emptinessTest True ex1and6
+            , emptinessTest False ex2and6
+            , emptinessTest False ex3and6
             ]
     ]
   
