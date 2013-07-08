1d6288bedb4ef83f6f3ce510a816815d76957b03
Zachary Palmer <zep_git@bahj.com>
2013-07-08 14:20:58 -0400

    Added some debugging logic to compatibility.

--- related-languages/pat-bang/lib-src/Language/PatBang/Interpreter/Compatibility.hs
+++ related-languages/pat-bang/lib-src/Language/PatBang/Interpreter/Compatibility.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE TupleSections, TemplateHaskell #-}
 
 {-|
   A module containing the compatibility relations for evaluation.
@@ -18,12 +18,16 @@ import Data.Set (Set)
 import qualified Data.Set as Set
 
 import Language.PatBang.Ast
+import Language.PatBang.Display
 import Language.PatBang.Interpreter.Basis
 import Language.PatBang.Interpreter.Patterns
 import Language.PatBang.Interpreter.Projection
+import Language.PatBang.Logging
 
 type Bindings = Map PatVar Value
 type PossibleBindings = Maybe Bindings
+
+$(loggingFunctions)
 
 -- |Calculates application compatibility for evaluation.  Unlike the
 --  specification, this function assumes that the *first* value in the list is
@@ -43,7 +47,7 @@ applicationCompatibility x1 scapes =
             do
               mvalues <- compatibilityTop x1 (pvars, pat)
               case mvalues of
-                Nothing -> return Nothing
+                Nothing -> applicationCompatibility x1 scapes'
                 Just values ->
                   if length values < length vvars
                     then applicationCompatibility x1 scapes'
@@ -51,7 +55,6 @@ applicationCompatibility x1 scapes =
                       let cls' = zipWith (ValueDef generated) vvars values
                       return $ Just $ Expr generated $ cls' ++ cls
           _ -> error "Projection gave inconsistent results"
-    _ : scapes' -> applicationCompatibility x1 scapes'
     _ -> return Nothing
     
 compatibilityTop :: FlowVar -> ([PatVar], PatternBody) -> EvalM (Maybe [Value])
@@ -70,43 +73,48 @@ compatibilityTop x (ys, pat) = do
 
 compatibility :: FlowVar -> PatternBody -> Set PatternBody
               -> EvalM PossibleBindings
-compatibility x pat visits = case pat of
-  PPrim _ p -> primCompat $ projPrim p
-  PLabel _ n pat' -> do
-    val <- project x $ SomeProjector $ projLabel n
-    case val of
-      Just (VLabel _ _ x') -> compatibility x' pat' Set.empty
-      Just _ -> error "Projection of label produced non-label value!"
-      Nothing -> return Nothing
-  PFun _ -> primCompat projFun
-  PPat _ -> primCompat projPat
-  PScape _ -> primCompat projScape
-  PConj _ pat' pat'' -> do
-    mb1 <- compatibility x pat' visits
-    mb2 <- compatibility x pat'' visits
-    case (mb1, mb2) of
-      (Just binds1, Just binds2) -> return $ Just $ Map.union binds2 binds1
-      (_, _) -> return Nothing
-  PSubst _ x' (AstList _ pats) -> checkVisits $ do
-    pattern <- project x' $ SomeProjector projPat
-    case pattern of
-      Just (VPattern _ (AstList _ ys) pat') ->
-        if length ys /= length pats
-          then evalError $ MismatchedPatternSubstitution ys pats
-          else
-            let pat'' = patternSubstitute (Map.fromList $ zip ys pats) pat' in
-            compatibility x pat'' $ Set.insert pat visits
-      Just _ -> error "Projection produced non-pattern for pattern projector"
-      Nothing -> return Nothing
-  PRec _ y pat' -> checkVisits $
-    let pat'' = patternSubstitute (Map.singleton y pat) pat' in
-    compatibility x pat'' $ Set.insert pat visits
-  PVar _ y -> Just . Map.singleton y <$> flowLookup x
-  where
-    primCompat :: Projector tag -> EvalM PossibleBindings
-    primCompat proj = do
-      mv <- project x $ SomeProjector proj
-      return $ if isNothing mv then Nothing else Just Map.empty
-    checkVisits :: EvalM PossibleBindings -> EvalM PossibleBindings
-    checkVisits ans = if pat `Set.member` visits
-                        then return $ Just Map.empty else ans
+compatibility x pat visits =
+  _debugI ("Checking compatibility for " ++ display x ++ " with "
+              ++ display pat) $
+  _debugValI ("Compatibility resul for " ++ display x ++ " with "
+              ++ display pat ++ " is: ") <$>
+  case pat of
+    PPrim _ p -> primCompat $ projPrim p
+    PLabel _ n pat' -> do
+      val <- project x $ SomeProjector $ projLabel n
+      case val of
+        Just (VLabel _ _ x') -> compatibility x' pat' Set.empty
+        Just _ -> error "Projection of label produced non-label value!"
+        Nothing -> return Nothing
+    PFun _ -> primCompat projFun
+    PPat _ -> primCompat projPat
+    PScape _ -> primCompat projScape
+    PConj _ pat' pat'' -> do
+      mb1 <- compatibility x pat' visits
+      mb2 <- compatibility x pat'' visits
+      case (mb1, mb2) of
+        (Just binds1, Just binds2) -> return $ Just $ Map.union binds2 binds1
+        (_, _) -> return Nothing
+    PSubst _ x' (AstList _ pats) -> checkVisits $ do
+      pattern <- project x' $ SomeProjector projPat
+      case pattern of
+        Just (VPattern _ (AstList _ ys) pat') ->
+          if length ys /= length pats
+            then evalError $ MismatchedPatternSubstitution ys pats
+            else
+              let pat'' = patternSubstitute (Map.fromList $ zip ys pats) pat' in
+              compatibility x pat'' $ Set.insert pat visits
+        Just _ -> error "Projection produced non-pattern for pattern projector"
+        Nothing -> return Nothing
+    PRec _ y pat' -> checkVisits $
+      let pat'' = patternSubstitute (Map.singleton y pat) pat' in
+      compatibility x pat'' $ Set.insert pat visits
+    PVar _ y -> Just . Map.singleton y <$> flowLookup x
+    where
+      primCompat :: Projector tag -> EvalM PossibleBindings
+      primCompat proj = do
+        mv <- project x $ SomeProjector proj
+        return $ if isNothing mv then Nothing else Just Map.empty
+      checkVisits :: EvalM PossibleBindings -> EvalM PossibleBindings
+      checkVisits ans = if pat `Set.member` visits
+                          then return $ Just Map.empty else ans
