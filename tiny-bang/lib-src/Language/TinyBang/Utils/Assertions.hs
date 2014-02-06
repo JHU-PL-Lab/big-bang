5cdf20880a373581495d8e9daa4487c7d1f74100
Zachary Palmer <zep_git@bahj.com>
2014-02-06 11:38:21 -0500

    Refined assertions module.

--- tiny-bang/lib-src/Language/TinyBang/Utils/Assertions.hs
+++ tiny-bang/lib-src/Language/TinyBang/Utils/Assertions.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 {-|
   A module for TinyBang assertion management.  There are two mechanisms that
   control whether TinyBang assertions are active.  The first is a static
@@ -11,6 +13,7 @@
 module Language.TinyBang.Utils.Assertions
 ( enableAssertions
 , assert
+, assertWithMessage
 ) where
 
 import Control.Applicative
@@ -18,6 +21,10 @@ import qualified Control.Exception as E
 import Control.Monad
 import Data.IORef
 import System.IO.Unsafe
+
+import Language.TinyBang.Utils.Logger
+
+$(loggingFunctions)
 
 assertionState :: IO (IORef Bool)
 assertionState = newIORef False
@@ -34,5 +41,31 @@ enableAssertions = do
     else ioError $ userError
             "Attempted to enable assertions, but assertions not compiled in."
 
+-- |Asserts a condition.  If this program was compiled with GHC assertions and
+--  the 'enableAssertions' routine has been called, this function will halt the
+--  program when the provided boolean is @False@.  Otherwise, this function
+--  ignores the argument and acts as identity.
 assert :: Bool -> a -> a
-assert b = E.assert (b && unsafePerformIO (join $ readIORef <$> assertionState))
+assert b =
+  E.assert $
+    -- Using if-then-else rather than short-circuiting operators here to make
+    -- our assumptions explicit.
+    if unsafePerformIO $ join $ readIORef <$> assertionState
+      then b
+      else True {- assertions are disabled; don't evaluate the check -}
+
+-- |Asserts a condition under the same circumstances as 'assert'.  If the
+--  assertion is raised, however, an error is logged immediately before the
+--  halt.  If assertions are not active, neither the message nor the boolean
+--  are evaluated.
+assertWithMessage :: String -> Bool -> a -> a
+assertWithMessage msg b =
+  E.assert $
+    -- Using if-then-else rather than short-circuiting operators here to make
+    -- our assumptions explicit.
+    if unsafePerformIO $ join $ readIORef <$> assertionState
+      then
+        if b
+          then True -- Assertion successful
+          else _errorI ("Assertion failure: " ++ msg) False -- Log then fail
+      else True -- assertions are disabled; don't evaluate the check
