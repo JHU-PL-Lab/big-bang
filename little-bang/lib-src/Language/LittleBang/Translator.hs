23540d6fb5640b58f56c9d28b0d08f71618685e2
Nely Jimenez <njimene7@jhu.edu>
2014-08-22 16:12:46 -0400

    Fixed expr[expr] issue. However, index out of bounds returns an empty list, whereas it should return an error message; this is a TODO.

--- little-bang/lib-src/Language/LittleBang/Translator.hs
+++ little-bang/lib-src/Language/LittleBang/Translator.hs
@@ -8,6 +8,7 @@ import Control.Monad.Reader
 
 import Language.LittleBang.Ast as LB
 import Language.TinyBang.Ast as TB
+import Language.TinyBangNested.Ast as TBN
 import Language.LittleBang.Lifter
 import Language.TinyBang.Utils.Syntax.Location (SourceDocument(UnknownDocument))
 import Language.TinyBangNested.Syntax.Lexer
@@ -478,6 +479,7 @@ desugarPatList pat =
       Nothing -> return (LB.LabelPattern o (LB.LabelName o "Nil") (LB.EmptyPattern o))
       Just p -> return p
 
+-- TODO: see why this is not working correctly.
 desugarLExprIndexedList :: LB.Expr -> DesugarM LB.Expr
 desugarLExprIndexedList expr = 
   case expr of
@@ -486,14 +488,44 @@ desugarLExprIndexedList expr =
     where
     getIndex :: TB.Origin -> LB.Expr -> Integer -> LB.Expr -> DesugarM LB.Expr
     getIndex o e i expr =
-      case e of
-        LB.TExprOnion _
-          (LB.TExprLabelExp _ (LB.LabelName _ "Hd") hd)
-          (LB.TExprLabelExp _ (LB.LabelName _ "Tl") tl) ->
-          if i == 0 then return hd
-          else if i < 0 then return expr -- TODO: this should throw an "index out of bounds" exception (currently it will throw a "Cannot TBN convert" error message.
-          else getIndex o tl (i - 1) expr
-        _ -> return expr                  
+      if i < 0 then return $
+        (LB.TExprLabelExp o  (LB.LabelName o "Nil") (LB.TExprValEmptyOnion o)) -- TODO: add "index out of bounds exception"; this is a temporary placeholder.
+      else  walkExprTree $ -- call desugar here
+        LB.TExprLet o
+          (LB.Ident o "obj")
+          (LB.LExprObject o [objTerm])
+          (LB.LExprDispatch o (LB.TExprVar o (LB.Ident o "obj")) (LB.Ident o "getElement")
+           [LB.NamedArg o (LB.Ident o "lst") e,
+            LB.NamedArg o (LB.Ident o "index") (LB.TExprValInt o i)])
+        where
+        objTerm = ObjectMethod o (LB.Ident o "getElement")
+          [LB.Param o (LB.Ident o "lst") (LB.EmptyPattern o), 
+           LB.Param o (LB.Ident o "index") (LB.PrimitivePattern o LB.PrimInt)]
+          (LB.TExprLet o (LB.Ident o "f")
+            (LB.TExprOnion o
+               (LB.LExprScape o [LB.Param o (LB.Ident o "l") 
+                  (LB.ConjunctionPattern o 
+                     (LB.LabelPattern o (LB.LabelName o "Hd") (LB.VariablePattern o (LB.Ident o "hd"))) 
+                     (LB.LabelPattern o (LB.LabelName o "Tl") (LB.VariablePattern o (LB.Ident o "tl"))))]
+                  (LB.LExprCondition o 
+                     (LB.TExprBinaryOp o (LB.TExprVar o (LB.Ident o "index")) (TBN.OpIntEq o) (LB.TExprValInt o 0)) 
+                     (LB.TExprVar o (LB.Ident o "hd")) 
+                     (LB.LExprDispatch o (LB.TExprVar o (LB.Ident o "self")) 
+                        (LB.Ident o "getElement") 
+                        [LB.NamedArg o (LB.Ident o "lst") (LB.TExprVar o (LB.Ident o "tl")),
+                         LB.NamedArg o (LB.Ident o "index") 
+                           (LB.TExprBinaryOp o (LB.TExprVar o (LB.Ident o "index")) 
+                           (TBN.OpIntMinus o)
+                           (LB.TExprValInt o 1))]))
+               ) 
+               (LB.LExprScape o [LB.Param o (LB.Ident o "l") 
+                  (LB.LabelPattern o (LB.LabelName o "Nil") (LB.EmptyPattern o))]
+                  (LB.TExprLabelExp o  (LB.LabelName o "Nil") (LB.TExprValEmptyOnion o)) -- TODO: add "index out of bounds exception"; this is a temporary placeholder.
+               )
+             )
+             (LB.LExprAppl o (LB.TExprVar o (LB.Ident o "f"))
+                [LB.NamedArg o (LB.Ident o "l") (LB.TExprVar o (LB.Ident o "lst"))])
+          )
 
 -- |Seal a given expression.  This function generates code which will apply the
 --  seal function to the provided expression. 
