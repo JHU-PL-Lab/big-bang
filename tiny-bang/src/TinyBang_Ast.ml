859edc182c850becfed2353b378ca785aa5e158e
Zachary Palmer <zep_git@bahj.com>
2015-05-06 21:01:34 -0400

    Basic AST implementation.  (No primitives, tracking data, etc.)

--- tiny-bang/src/TinyBang_Ast.ml
+++ tiny-bang/src/TinyBang_Ast.ml
@@ -1,18 +1,40 @@
+open Batteries;;
+
 module TinyBang_Ast =
 struct
-(*	
-	type expr = Expr of clause list;;
-	
-	type clause = Clause of var * redex;;
-	
-	type redex = ValueRedex of value | VarRedex of var | ApplRedex of var * var;;
-	
-	type var = Var of ident;;
-	
-	type ident = Ident of string;;
-
-type value = EmptyOnionValue | LabelValue of label * var | OnionValue of var * var | FunctionValue of pattern * expr;;
-
-type pattern = Pattern of var * patfilter
-	*)
+  type ident = Ident of string;;
+  
+  type label = Label of ident;;
+  
+  type var = Var of ident;;
+  
+  type patFilter =
+    | EmptyFilter
+    | LabelFilter of label * var
+    | ConjunctionFilter of var * var;;
+  
+  type patFilterRule = PatFilterRule of var * patFilter;;
+  
+  module PatternFilterRuleSet = Set.Make(
+    struct
+      type t = patFilterRule
+      let compare = compare
+    end);;
+  
+  type pattern = Pattern of var * PatternFilterRuleSet.t;;
+  
+  type expr = Expr of clause list
+  
+  and clause = Clause of var * redex
+  
+  and	redex =
+    | ValueRedex of value
+    | VarRedex of var
+    | ApplRedex of var * var
+  
+  and	value =
+    | EmptyOnionValue
+    | LabelValue of label * var
+    | OnionValue of var * var
+    | FunctionValue of pattern * expr;;
 end;;
