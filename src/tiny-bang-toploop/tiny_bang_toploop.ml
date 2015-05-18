4839f5c5a61141c4f2b493e95397a148610c3c58
Zachary Palmer <zep_git@bahj.com>
2015-05-18 16:40:36 -0400

    Added typechecking to toploop.

--- src/tiny-bang-toploop/tiny_bang_toploop.ml
+++ src/tiny-bang-toploop/tiny_bang_toploop.ml
@@ -2,16 +2,29 @@ open Batteries;;
 
 open Tiny_bang_ast_pretty;;
 open Tiny_bang_interpreter;;
+open Tiny_bang_typechecker;;
 
 let toploop_operate e =
   print_string "\n";
-  let v,env = eval e in
-  print_string (pretty_var v ^ " where "  ^ pretty_env env ^ "\n");
+  (if typecheck e
+    then
+      let v,env = eval e in
+      print_string (pretty_var v ^ " where "  ^ pretty_env env ^ "\n");
+      print_string "\n"
+    else
+      print_string "Type error.\n");
+  print_string "Please enter an expression to evaluate followed by \";;\".\n";
   print_string "\n";
   flush stdout
 ;;
 
 let () =
+  print_string "TinyBang 0.3 Toploop\n";
+  print_string "--------------------\n";
+  print_string "\n";
+  print_string "Please enter an expression to evaluate followed by \";;\".\n";
+  print_string "\n";
+  flush stdout;
   Tiny_bang_parser.parse_tiny_bang_expressions IO.stdin
     |> LazyList.map fst
     |> LazyList.iter toploop_operate
