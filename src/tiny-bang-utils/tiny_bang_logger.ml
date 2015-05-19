7c046a525ae66a841182d91740d86996c527c4db
Leandro Facchinetti <me@leafac.com>
2015-05-19 15:55:23 -0400

    Add support for setting log levels as command line parameters

--- src/tiny-bang-utils/tiny_bang_logger.ml
+++ src/tiny-bang-utils/tiny_bang_logger.ml
@@ -48,18 +48,73 @@
  * ---
  *
  * WARNING: Be aware that, due to I/O buffering, log entries may not show up on
- *          the terminal as soon as you might expect.
+ *          the terminal as soon as you might expect. To solve that, you might want
+ *          to call `flush stdout'.
  *
  * [1]: http://12factor.net/logs
  *)
 
 open Batteries
 
-let level level =
-  BatLog.Easy.level := level
+let default_level =
+  let rec parse_command_line_parameters arguments result =
+    match arguments with
+    | [] -> result
+    | [single_parameter] -> if single_parameter = "--log" then
+                              failwith "Missing log level after `--log'."
+                            else
+                              result
+    | flag :: argument :: arguments_tail ->
+       if flag = "--log" then
+         if BatString.exists argument "=" then
+           result
+         else
+           argument
+       else
+         parse_command_line_parameters (argument :: arguments_tail) result
+  in
+  parse_command_line_parameters (BatArray.to_list Sys.argv) "warn"
+;;
+let level_map =
+  let rec parse_command_line_parameters arguments result =
+    match arguments with
+    | [] -> result
+    | [single_parameter] -> if single_parameter = "--log" then
+                              failwith "Missing log level after `--log'."
+                            else
+                              result
+    | flag :: argument :: arguments_tail ->
+       if flag = "--log" then
+         if BatString.exists argument "=" then
+           let (module_name, level_string) = BatString.split argument "=" in
+           BatMap.add module_name level_string result
+         else
+           result
+       else
+         parse_command_line_parameters (argument :: arguments_tail) result
+  in
+  parse_command_line_parameters (BatArray.to_list Sys.argv) BatMap.empty
+;;
+
+let level_for prefix =
+  let level_string =
+    if BatMap.mem prefix level_map then
+      BatMap.find prefix level_map
+    else
+      default_level
+  in match level_string with
+     | "trace" -> `trace
+     | "debug" -> `debug
+     | "info" -> `info
+     | "warn" -> `warn
+     | "error" -> `error
+     | "fatal" -> `fatal
+     | "always" -> `always
+     | _ -> failwith ("Invalid log level `" ^ level_string ^ "'.")
 ;;
 
 let make_logger prefix level message =
+  BatLog.Easy.level := level_for prefix;
   BatLog.Easy.log level ("[" ^ prefix ^ "]: " ^ message)
 ;;
 
