ef20d0c41610a728117f7dcab4e8908eb94741f7
Zachary Palmer <zep_git@bahj.com>
2015-05-20 13:41:29 -0400

    Added pretty printing utility functions.

--- src/tiny-bang-utils/tiny_bang_string_utils.ml
+++ src/tiny-bang-utils/tiny_bang_string_utils.ml
@@ -12,3 +12,13 @@ let concat_sep sep strs =
 let concat_sep_delim start stop sep strs =
   start ^ concat_sep sep strs ^ stop
 ;;
+
+let pretty_list : 'a. ('a -> string) -> 'a list -> string =
+  fun pretty_el lst ->
+    concat_sep_delim "[" "]" ";" @@ List.enum @@ List.map pretty_el lst
+;;
+
+let pretty_tuple : 'a 'b. ('a -> string) -> ('b -> string) -> 'a * 'b -> string
+  = fun pretty_a pretty_b (a,b) ->
+    "(" ^ pretty_a a ^ ", " ^ pretty_b b ^ ")"
+;;
